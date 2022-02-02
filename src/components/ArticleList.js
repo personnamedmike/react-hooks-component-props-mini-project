bc92471a1adeaedd2876299f360456e4a18ff0f0
Mike Starr <imhs@protonmail.com>
2022-02-01 16:43:41 -0800

    Completed Assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  console.log(posts);
+  return (
+    <>
+      <Article />
+      <main>
+        {posts.map((post) => {
+          return (
+            <Article
+              key={post.id}
+              title={post.title}
+              date={post.date}
+              preview={post.preview}
+            />
+          );
+        })}
+      </main>
+    </>
+  );
+}
+
+export default ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import blogData from "../data/blog";
+
+function Header({ name }) {
+  return (
+    <>
+      <header>
+        <h1>{name}</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
